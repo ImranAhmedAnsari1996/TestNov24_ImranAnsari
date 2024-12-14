ab639cc7ba90ca336e995d2f43f22acbf017b923
Imran Ansari <imranahmedansari8987@gmail.com>
2024-12-14 12:02:22 +0530

    Second Scenario Branch A

--- src/practice/PracClass1.java
+++ src/practice/PracClass1.java
@@ -4,7 +4,7 @@ public class PracClass1 {
 
 	int k=15;
 	public static void main(String[] args) {
-		System.out.println("Hello_class1");
+		System.out.println("Hello_class1, branch A changes for Second Scenario");
 		System.out.println("Good day");
 		System.out.println("Changes done by A");
 	}
