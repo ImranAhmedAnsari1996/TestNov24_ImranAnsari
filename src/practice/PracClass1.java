343345f61ca5cffbce7f19d92218a479deb35a32
Imran Ansari <imranahmedansari8987@gmail.com>
2024-12-14 12:05:42 +0530

    Updated

--- src/practice/PracClass1.java
+++ src/practice/PracClass1.java
@@ -4,7 +4,7 @@ public class PracClass1 {
 
 	int k=15;
 	public static void main(String[] args) {
-		System.out.println("Hello_class1");
+		System.out.println("Hello_class1, second scenarion by B");
 		System.out.println("Good day");
 		System.out.println("Changes done by A, now by B");
 	}
