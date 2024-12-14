40f952f1d9aceee13be8851d74ebdd03668bc55e
Imran Ansari <imranahmedansari8987@gmail.com>
2024-12-14 11:43:48 +0530

    Branch B updated

--- src/practice/PracClass1.java
+++ src/practice/PracClass1.java
@@ -6,6 +6,6 @@ public class PracClass1 {
 	public static void main(String[] args) {
 		System.out.println("Hello_class1");
 		System.out.println("Good day");
-		System.out.println("Changes done by A");
+		System.out.println("Changes done by A, now by B");
 	}
 }
